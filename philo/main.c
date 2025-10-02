511c8f71502c4537046a9d6825f3bde823d5c5da
Sato Ryuu Leonardo <leonardo@stepai.co.jp>
2025-10-02 18:35:42 +0900

    ヘッダ定義/init準備

--- philo/main.c
+++ philo/main.c
@@ -12,3 +12,15 @@
 
 #include "philo.h"
 
+int main(int argc, char **argv)
+{
+	t_obs *obs;
+
+	obs = malloc(sizeof(t_obs));
+	if(obs == NULL)
+		return(EXIT_FAILURE);
+	if(init_philos(argc, argv, obs) == -1)
+		return(free(obs), EXIT_FAILURE);//ここinit側でphilosをmallocしたらfree
+
+	return(EXIT_SUCCESS);
+}
--- philo/philo.h
+++ philo/philo.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rysato <rysato@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/27 16:22:44 by rysato            #+#    #+#             */
-/*   Updated: 2025/09/27 19:51:03 by rysato           ###   ########.fr       */
+/*   Created: 2025/10/02 18:29:01 by rysato            #+#    #+#             */
+/*   Updated: 2025/10/02 18:29:01 by rysato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,32 @@ typedef struct s_conf
 	int	t_sleep;
 	int	must_eat;
 }		t_conf;
+
+typedef struct s_philo
+{
+	int				id;
+	int				leftid;
+	int				rightid;
+	int				lastmeal_ms;
+	int				meals;
+	pthread_t		th;
+	t_obs			*obs;
+	pthread_mutex_t	*lfork;
+	pthread_mutex_t	*rfork;
+	pthread_mutex_t	*meal_mx;
+}					t_philo;
+
+typedef struct s_obs
+{
+	t_conf			conf;
+	long			start_ms;
+	volatile int	stop;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*meal_mx;
+	pthread_mutex_t	*print_mx;;
+	pthread_mutex_t	*state_mx;
+	t_philo			*ph;
+	pthread_t		monitor_th;
+}					t_obs;
 
 #endif
